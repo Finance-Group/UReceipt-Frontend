b0cca9acbfc5763047f05a12b539ebc27a140e01
WilmarTarazona <alonso_tch@hotmail.com>
2021-11-26 11:47:23 -0500

    Crear componente crearRecibos

---
+++ src/app/components/crear-recibos/crear-recibos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-recibos',
+  templateUrl: './crear-recibos.component.html',
+  styleUrls: ['./crear-recibos.component.css']
+})
+export class CrearRecibosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

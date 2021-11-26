c66a0f63b2d4e9440eb4febc0840e27fa3597764
WilmarTarazona <alonso_tch@hotmail.com>
2021-11-26 08:32:22 -0500

    Actualizar historial.ts

--- src/app/components/historial/historial.component.ts
+++ src/app/components/historial/historial.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AppService } from "../../services/app.service";
+import {Cartera} from "../../models/cartera";
 
 @Component({
   selector: 'app-historial',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistorialComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AppService: AppService, private router: Router) { }
+
+  personId: string
+  listCarteras: Cartera[]
 
   ngOnInit(): void {
+    this.personId = this.AppService.getToken()
   }
 
+  getCarterasByPersonId() {
+
+  }
 }
